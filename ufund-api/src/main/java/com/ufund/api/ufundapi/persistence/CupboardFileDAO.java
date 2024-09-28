ddb3593676c5b5750d9f93466dbba6eb85513d10
Leo Mai <lm2029@rit.edu>
2024-09-28 03:20:30 -0400

    Added CupboardDAO's instance structure. New methods to load and save file changes. Filled out method headers and internal structures for loading the file, creating, updating, getting, and deleting needs.

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/CupboardFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/CupboardFileDAO.java
@@ -1,27 +1,99 @@
 package com.ufund.api.ufundapi.persistence;
 
 import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.ufund.api.ufundapi.model.Need;
+import com.fasterxml.jackson.databind.ObjectMapper
+
 
 public class CupboardFileDAO implements CupboardDAO{
-    
+
+    /**
+     * CupboardFileDao -  Current instance of the cupboard data access.
+     *
+     * @param filename - The name of the file to be loaded.
+     * @param objectmapper - The object mapper.
+     */
+    // not sure how to recieve file name.
+    public CupboardFileDAO(String filename, ObjectMapper objectmapper) throws IOException{
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+        this.needs = new HashMap<>():
+        loadFile();
+    }
+
+    /**
+     * loadFile - Loads the needs into a hashmap, where the key is the ID, the value is the full need structure.
+     * @throws IOException
+     */
+    private void loadFile() throws IOException{
+        Need[] needList = objectMapper.readValue(new File(filename),Need[].class);
+        for (Need currNeed : needList){ // for each need, load hashmap of needs with ID and need structure.
+            needs.put(currNeed.getId(), need);
+        }
+    }
+
+    /**
+     * saveFile - Saves the hashmap
+     * @throws IOException
+     */
+    private void saveFile() throws IOException{
+
+    }
+
+    /**
+     * CreateNeed - Adds the need parameter into the needs hashmap.
+     * @param need - New need to be added
+     * @return - If need works, return need. If not, null.
+     * @throws IOException
+     */
     @Override
     public Need CreateNeed(Need need) throws IOException {
         // TODO Auto-generated method stub
-        return null;
+        if(needs.containsKey(need.getId())){
+            return null; // if the need exists, do nothing.
+        } else {
+            this.needs.put(need.getId(), need);
+            // might need to save new need
+            return need;
+        }
     }
 
+    /**
+     * UpdateNeed - If the need parameter's ID exists, the need is replaced.
+     * @param need - This is the need being updated.
+     * @return - Parameter need if successful, null if not.
+     * @throws IOException
+     */
     @Override
     public Need UpdateNeed(Need need) throws IOException {
         // TODO Auto-generated method stub
-        return null;
+        if(this.needs.containsKey(need.getId())) {
+            this.needs.put(need.getId(), need);
+            return need;
+        } else {
+            return null;
+        }
     }
-    
+
+    /**
+     * GetNeed - Returns the need if it exists.
+     * @param id - Tnput need ID
+     * @return Either found need, or null.
+     * @throws IOException
+     */
     @Override
     public Need GetNeed(int id) throws IOException {
         // TODO Auto-generated method stub
-        return null;
+            if (this.needs.containsKey(id)){
+                return this.needs.get(id);
+            }
+            else{
+                return null;
+            }
     }
 
     @Override
@@ -36,9 +108,22 @@ public class CupboardFileDAO implements CupboardDAO{
         return null;
     }
 
+    /**
+     * DeleteNeed - Deletes the need if it's ID exists.
+     * @param id - ID of need looking to be remove.
+     * @return - True if need exists, false if it does not.
+     * @throws IOException
+     */
     @Override
     public boolean DeleteNeed(int id) throws IOException {
         // TODO Auto-generated method stub
-        return false;
+        if (this.needs.containsKey(id)){
+            this.needs.remove(id);
+            return true;
+            // might need to save change.
+        }
+        else{
+            return false;
+        }
     }
 }
