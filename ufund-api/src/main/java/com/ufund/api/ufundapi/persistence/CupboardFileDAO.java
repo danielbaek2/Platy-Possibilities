8a7145d1bd9e7195ee9ffb5f45552f201afd5d38
jba7308 <jba7308@rit.edu>
2024-09-27 23:18:56 -0400

    added method stubs for all inherited methods

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/CupboardFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/CupboardFileDAO.java
@@ -1,5 +1,44 @@
 package com.ufund.api.ufundapi.persistence;
 
+import java.io.IOException;
+
+import com.ufund.api.ufundapi.model.Need;
+
 public class CupboardFileDAO implements CupboardDAO{
     
+    @Override
+    public Need CreateNeed(Need need) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Need UpdateNeed(Need need) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public Need GetNeed(int id) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Need[] GetNeeds() throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Need[] SearchNeeds(String substring) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean DeleteNeed(int id) throws IOException {
+        // TODO Auto-generated method stub
+        return false;
+    }
 }
