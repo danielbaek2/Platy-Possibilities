6d922b8abf94a33273480c66d227423da7c53d15
Leo Mai <lm2029@rit.edu>
2024-09-29 14:13:32 -0400

    .

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
@@ -2,4 +2,31 @@ package com.ufund.api.ufundapi.controller;
 
 public class CupboardController {
 
+
+    /**
+     * Updates the {@linkplain Hero hero} with the provided {@linkplain Hero hero} object, if it exists
+     *
+     * @param hero The {@link Hero hero} to update
+     *
+     * @return ResponseEntity with updated {@link Hero hero} object and HTTP status of OK if updated<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PutMapping("")
+    public ResponseEntity<Need> updateNeed(@RequestBody Need need) {
+        LOG.info("PUT /heroes " + hero);
+
+        // Replace below with your implementation
+        try{
+            Need uNeed = needDao.updateNeed(need);
+            if (uNeed != null) {
+                return new ResponseEntity<>(uNeed, HttpStatus.OK);
+            } else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
