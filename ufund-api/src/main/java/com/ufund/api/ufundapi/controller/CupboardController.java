45ba339e8a78f772f57e599af2a90f44bd3c158e
Kaila Grant <kailagrant141@gmail.com>
2024-09-29 14:33:51 -0400

    created api for getting the entire cupboard (GetNeeds)

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
@@ -1,5 +1,6 @@
 package com.ufund.api.ufundapi.controller;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -34,5 +35,118 @@ public class CupboardController {
      */
     public CupboardController(CupboardDAO boardDAO) {
         this.boardDAO = boardDAO;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Helper method to return the correct object and HTTP status
+     * 
+     * @param <T> Generic parameter, allowing method to work with Need objects and arrays
+     * @param input Need object/array to be returned
+     * @param error_status The HTTP Status to be returned if the object is null
+     * @return ResponseEntity with the {@linkplain Need need} object or array and 
+     * HTTP status of OK if found, HTTP status of error_status if not found or if there is a conflict
+     */
+    private <T> ResponseEntity<T> serviceClass(T input, HttpStatus errorStatus){
+        if (input != null)
+            return new ResponseEntity<T>(input,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(errorStatus);
+    }
+
+    /**
+     * Responds to the GET request for all {@linkplain Need needs}
+     * 
+     * @return ResponseEntity with array of {@linkplain Need need} objects and 
+     * HTTP status of OK if found, HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("")
+    public ResponseEntity<Need[]> GetNeeds() {
+        LOG.info("GET/needs");
+
+        try {
+            Need[] needs = boardDAO.GetNeeds();
+            return serviceClass(needs, HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }    
     }
 }
