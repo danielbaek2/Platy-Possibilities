8c639d7da072383d4c8eb082baebb41a3af3e2c4
Leo Mai <lm2029@rit.edu>
2024-09-29 14:41:14 -0400

    .

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
@@ -37,60 +37,23 @@ public class CupboardController {
     }
 
 
-    /**
-     * 
-     * @param need the need object to create
-     * 
-     * @return a Response Entity with the need and HttpStatus.Created if successful
-     * @return a Response Entity with HttpStatus.INTERNAL_SERVER_ERROR if creation unsuccessful or IO exception occurred
-     */
-    @PostMapping("")
-    public ResponseEntity<Need> createNeed(@RequestBody Need need){
-        LOG.info("POST /Cupboard " + need);
 
-        try{
-            Need newNeed = boardDAO.CreateNeed(need); //try to create a need
-            if(newNeed != null){
-                return new ResponseEntity<Need>(newNeed, HttpStatus.CREATED);//return status created and need if successful
-            }
 
-            else{
-                return new ResponseEntity<Need>(HttpStatus.INTERNAL_SERVER_ERROR);//return status Server Error and if unsuccessful
-            }
-        }
-
-      
-      
-
-        catch(IOException error){
-            return new ResponseEntity<Need>(HttpStatus.INTERNAL_SERVER_ERROR);//return status Server Error and if IO exception occurred.
-        }
-    }
-
-    /**
-     * Updates the {@linkplain Hero hero} with the provided {@linkplain Hero hero} object, if it exists
-     *
-     * @param hero The {@link Hero hero} to update
-     *
-     * @return ResponseEntity with updated {@link Hero hero} object and HTTP status of OK if updated<br>
-     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
-     */
-    @PutMapping("")
-    public ResponseEntity<Need> updateNeed(@RequestBody Need need) {
-        LOG.info("PUT /heroes " + hero);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Need> deleteNeed(@PathVariable int id) {
+        LOG.info("DELETE /need/" + id);
 
         // Replace below with your implementation
         try {
-            Need uNeed = needDao.updateNeed(need);
-            if (uNeed != null) {
-                return new ResponseEntity<>(uNeed, HttpStatus.OK);
+            boolean delete = needDao.deleteNeed(id);
+            if (delete) {
+                return new ResponseEntity<>(HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
             LOG.log(Level.SEVERE, e.getLocalizedMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
         }
     }
 }
