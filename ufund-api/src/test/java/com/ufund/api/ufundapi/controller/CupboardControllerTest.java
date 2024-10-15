8ce17a27a1fae1104991852891cab9231032aeee
KinkadeKS <kinkadejknoxsilvia@gmail.com>
2024-10-15 17:25:05 -0400

    created class for cupboard controller tests

---
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/controller/CupboardControllerTest.java
@@ -0,0 +1,35 @@
+package com.ufund.api.ufundapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class CupboardControllerTest {
+    @Test
+    void testGetNeeds() {
+
+    }
+
+    @Test
+    void testCreateNeed() {
+
+    }
+
+    @Test
+    void testDeleteNeed() {
+
+    }
+
+    @Test
+    void testGetNeed() {
+
+    }
+
+    @Test
+    void testSearchNeeds() {
+
+    }
+
+    @Test
+    void testUpdateNeed() {
+
+    }
+}
