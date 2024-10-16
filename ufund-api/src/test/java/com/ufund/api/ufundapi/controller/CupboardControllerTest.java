e0ea006449ecd331f2170119667e2ad70a6054df
KinkadeKS <kinkadejknoxsilvia@gmail.com>
2024-10-16 09:53:12 -0400

    getNeedsTest

--- ufund-api/src/test/java/com/ufund/api/ufundapi/controller/CupboardControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/controller/CupboardControllerTest.java
@@ -1,11 +1,32 @@
 package com.ufund.api.ufundapi.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+import java.io.IOException;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.ufund.api.ufundapi.model.Need;
+import com.ufund.api.ufundapi.persistence.CupboardDAO;
 
 public class CupboardControllerTest {
     @Test
-    void testGetNeeds() {
-        
+    void testGetNeeds() throws IOException {
+        CupboardDAO mockDAO = mock(CupboardDAO.class);
+        Need mockNeed = mock(Need.class);
+        CupboardController controller = new CupboardController(mockDAO);
+
+        Need[] needArray = {mockNeed};
+        when(mockDAO.getNeeds()).thenReturn(needArray);
+        ResponseEntity<Need[]> expected = new ResponseEntity<>(needArray,HttpStatus.OK);
+
+        assertEquals(expected,controller.getNeeds());
     }
 
     @Test
