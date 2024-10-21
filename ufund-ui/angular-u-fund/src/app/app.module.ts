3b7f60ebd0619948d3ece090d230e202ec051f87
Daniel Baek <db7978@g.rit.edu>
2024-10-21 15:43:20 -0400

    generated need component

--- ufund-ui/angular-u-fund/src/app/app.module.ts
+++ ufund-ui/angular-u-fund/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NeedComponent } from './need/need.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NeedComponent
   ],
   imports: [
     BrowserModule,
