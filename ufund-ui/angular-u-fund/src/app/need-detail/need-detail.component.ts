08581035810e226e8d62ffe5be0854470c536abc
Daniel Baek <db7978@g.rit.edu>
2024-10-21 20:42:25 -0400

    updated need detail

--- ufund-ui/angular-u-fund/src/app/need-detail/need-detail.component.ts
+++ ufund-ui/angular-u-fund/src/app/need-detail/need-detail.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Need } from '../need';
+import { FormsModule } from '@angular/forms';
+import { NgIf, UpperCasePipe } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-need-detail',
   templateUrl: './need-detail.component.html',
-  styleUrl: './need-detail.component.css'
+  styleUrl: './need-detail.component.css',
+  imports: [FormsModule, NgIf, UpperCasePipe],
 })
 export class NeedDetailComponent {
-
+  @Input() need?: Need;
 }
