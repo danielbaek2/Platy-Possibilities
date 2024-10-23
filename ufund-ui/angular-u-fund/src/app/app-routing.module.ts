a6ebb16640078b8e5ed1ffcf8c43d1ea57b91bda
jba7308 <jba7308@rit.edu>
2024-10-23 18:24:36 -0400

    added routing to login page

--- ufund-ui/angular-u-fund/src/app/app-routing.module.ts
+++ ufund-ui/angular-u-fund/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { NeedComponent } from './need/need.component';
 import { HelperComponent } from './helper/helper.component';
 import { NeedDetailComponent } from './need-detail/need-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'needs', component: NeedComponent },
   { path: 'helper', component: HelperComponent },
   { path: '', redirectTo: '/helper', pathMatch: 'full' },
   { path: 'detail/:id', component: NeedDetailComponent },
+  { path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
