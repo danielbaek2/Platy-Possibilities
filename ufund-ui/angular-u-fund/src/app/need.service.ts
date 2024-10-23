4ffaacffca7d6521dade886e44b8fd8654460785
Daniel Baek <db7978@g.rit.edu>
2024-10-22 22:50:49 -0400

    fixed addNeed method so it accounts for adding a need that already exists

--- ufund-ui/angular-u-fund/src/app/need.service.ts
+++ ufund-ui/angular-u-fund/src/app/need.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Need } from './need';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NeedService {
-  private needsUrl = 'http://localhost:8080/Cupboard';  // URL to web api
+  private needsUrl = 'api/needs';  // URL to web api http://localhost:8080/Cupboard
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,7 +18,9 @@ export class NeedService {
   constructor(private messageService: MessageService, private http: HttpClient) { }
 
   getNeeds(): Observable<Need[]> {
-    return this.http.get<Need[]>(this.needsUrl).pipe(tap(_ => this.log('fetched needs' )), catchError(this.handleError<Need[]>('getNeeds', [])));
+    return this.http.get<Need[]>(this.needsUrl).pipe(
+      tap(_ => this.log('fetched needs' )), 
+      catchError(this.handleError<Need[]>('getNeeds', [])));
   }
 
   getNeedNo404<Data>(id: number): Observable<Need> {
@@ -32,23 +34,43 @@ export class NeedService {
 
   getNeed(id: number): Observable<Need> {
     const url = `${this.needsUrl}/${id}`;
-    return this.http.get<Need>(url).pipe(tap(_ => this.log(`fetched need id=${id}`)), catchError(this.handleError<Need>(`getNeed id=${id}`)));
+    return this.http.get<Need>(url).pipe(
+      tap(_ => this.log(`fetched need id=${id}`)), 
+      catchError(this.handleError<Need>(`getNeed id=${id}`)));
   }
 
   /** PUT: update the Need on the server */
   updateNeed(need: Need): Observable<any> {
-    return this.http.put(this.needsUrl, need, this.httpOptions).pipe(tap(_ => this.log(`updated need id=${need.id}`)), catchError(this.handleError<any>('updateNeed')));
+    return this.http.put(this.needsUrl, need, this.httpOptions).pipe(
+      tap(_ => this.log(`updated need id=${need.id}`)), 
+      catchError(this.handleError<any>('updateNeed')));
   }
 
   /** POST: add a new Need to the server */
   addNeed(need: Need): Observable<Need> {
-    return this.http.post<Need>(this.needsUrl, need, this.httpOptions).pipe(tap((newNeed: Need) => this.log(`added need w/ id=${newNeed.id}`)), catchError(this.handleError<Need>('addNeed')));
+    return this.getNeeds().pipe(
+      map(needs => {
+        const titleExists = needs.some(existingNeed => existingNeed.title === need.title);
+        if (titleExists) {
+          throw new Error(`Error: Need "${need.title}" already exists!`);
+        }
+        return need;
+      }),
+      switchMap(needToAdd => 
+        this.http.post<Need>(this.needsUrl, needToAdd, this.httpOptions).pipe(
+          tap((newNeed: Need) => this.log(`added need w/ id=${newNeed.id}`)),
+          catchError(this.handleError<Need>('addNeed'))
+        )
+      )
+    );
   }
 
   /** DELETE: delete the Need from the server */
   deleteNeed(id: number): Observable<Need> {
     const url = `${this.needsUrl}/${id}`;
-    return this.http.delete<Need>(url, this.httpOptions).pipe(tap(_ => this.log(`deleted need id=${id}`)), catchError(this.handleError<Need>('deleteNeed')));
+    return this.http.delete<Need>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted need id=${id}`)), 
+      catchError(this.handleError<Need>('deleteNeed')));
   }
 
   /* GET Needs whose title contains search term */
