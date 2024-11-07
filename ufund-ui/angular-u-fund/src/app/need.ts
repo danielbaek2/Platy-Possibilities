4f0ac1b8dc00514f1e867f8d699d2eee6ec7b83f
lmai2029 <lm2029@rit.edu>
2024-11-07 11:23:09 -0500

    Fixed descriptions

--- ufund-ui/angular-u-fund/src/app/need.ts
+++ ufund-ui/angular-u-fund/src/app/need.ts
@@ -1,6 +1,8 @@
 export interface Need {
   id: number;
   title: string;
-  description: string;
-  total_funding: number;
-  }
+  description: string | null;
+  quantity: number;
+  cost: number;
+  quantityFunded: number;
+}
