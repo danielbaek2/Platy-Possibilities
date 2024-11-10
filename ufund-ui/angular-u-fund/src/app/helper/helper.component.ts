0b36f5b8b9f705d285567d99b29e7cc0ad269236
jba7308 <jba7308@rit.edu>
2024-11-10 15:59:16 -0500

    fixed spelling mistake and incorrect file path

--- ufund-ui/angular-u-fund/src/app/helper/helper.component.ts
+++ ufund-ui/angular-u-fund/src/app/helper/helper.component.ts
@@ -47,7 +47,7 @@ export class HelperComponent implements OnInit {
     this.selectedNeeds.push(need);
   }
 
-  addMultileToBasket(): void{
+  addMultipleToBasket(): void{
     this.selectedNeeds.forEach(need => this.addNeedToBasket(need))
   }
 
